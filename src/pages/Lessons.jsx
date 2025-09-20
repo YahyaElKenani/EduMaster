b60ceb811e80b1e6e5cd9b5e270cadafb9f28c1c
Yahya El Kenani <yahyamahmoud887@gmail.com>
2025-09-20 05:06:23 +0300

    feat: added loader to lessons page

--- src/pages/Lessons.jsx
+++ src/pages/Lessons.jsx
@@ -4,12 +4,14 @@ import Header from "../components/Header";
 import { AuthContext } from "../context/context";
 import { PiBookOpenTextBold } from "react-icons/pi";
 import LessonCard from "../components/LessonCard";
+import { HashLoader } from "react-spinners";
 export default function Lessons() { 
     const {token} = useContext(AuthContext);
     const [lessons, setLessons] = useState(null);
     const [grade, setGrade] = useState();
     const [search, setSearch] = useState('');
     const [filteredLessons, setFilteredLessons] = useState([]); 
+    const [loading, setLoading] = useState(true);
     const getGrade = async () => { 
         const response = await fetch ('https://edu-master-delta.vercel.app/user/', { 
             headers: { 
@@ -45,55 +47,68 @@ export default function Lessons() {
         }
     }, [search, lessons]) 
 
-    useEffect(() => {console.log(lessons);}, [lessons])
+    useEffect(() => {
+        if (lessons && grade) { 
+            setLoading(false);
+        }
+    }, [lessons, grade])
     return ( 
         <div className="flex flex-col min-h-screen"> 
             <Header />
                 <div className="flex-grow"> 
-                    <div className="bg-[var(--primary-bg)] p-12 flex flex-col items-center md:flex-row md:justify-between w-full gap-4"> 
-                    <div className="flex items-center gap-3">
-                        <PiBookOpenTextBold size={24}/>
-                        <div className="flex flex-col gap-1"> 
-                            <h1 className="text-2xl font-bold">Lessons</h1>
-                            <h3 className="text-black/40">Browse interactive lessons</h3>
-                        </div>
-                    </div>
+                    { 
+                        loading ? 
+                        <div className="flex items-center justify-center min-h-[70dvh] w-full"> 
+                            <HashLoader color="var(--primary)"/>
+                        </div> 
+                        : 
+                        <> 
+                            <div className="bg-[var(--primary-bg)] p-12 flex flex-col items-center md:flex-row md:justify-between w-full gap-4"> 
+                                <div className="flex items-center gap-3">
+                                    <PiBookOpenTextBold size={24}/>
+                                    <div className="flex flex-col gap-1"> 
+                                        <h1 className="text-2xl font-bold">Lessons</h1>
+                                        <h3 className="text-black/40">Browse interactive lessons</h3>
+                                    </div>
+                                </div>
 
-                    <div className="flex flex-col md:flex-row gap-5 w-1/2 justify-end items-center"> 
-                        <input type="search" placeholder="Search lessons..." className="p-3 h-12 md:w-1/3 rounded-xl bg-gray-100"
-                        onChange={(e) => setSearch(e.target.value)}
-                        />
-                        <button className="bg-[var(--primary)] min-w-[100px] p-3 rounded-xl text-white cursor-pointer hover:opacity-70 
-                        transition-all duration-200 ease-linear w-fit disabled:cursor-not-allowed" 
-                        disabled={search ? false : true}
-                        onClick={() => searchLessons()}
-                        >Search</button>
-                        <div className="flex flex-col justify-center items-center p-3 h-24 w-fit rounded-xl bg-gray-100">
-                            <div className="font-bold">Level:</div>
-                            <div className="text-center">{grade ? grade : 'Loading...'}</div>
-                        </div>
-                    </div>
-                </div>
-
-                    <div className="flex flex-col p-12 gap-4 items-center"> 
-                        <h1 className="text-3xl font-bold">{grade ? grade : 'Your'} Lessons</h1>
-                        <div className="w-full flex flex-wrap justify-center gap-4">
-                            { 
-                                filteredLessons && 
-                                filteredLessons?.map((lesson) => ( 
-                                    <LessonCard
-                                    title={lesson.title}
-                                    description={lesson.description}
-                                    tags={[grade, lesson.isPaid ? 'Paid' : 'Free']}
-                                    price={lesson.price}
-                                    key={lesson._id}
-                                    lessonId={lesson._id}
-                                    token={token}
+                                <div className="flex flex-col md:flex-row gap-5 w-1/2 justify-end items-center"> 
+                                    <input type="search" placeholder="Search lessons..." className="p-3 h-12 md:w-1/3 rounded-xl bg-gray-100"
+                                    onChange={(e) => setSearch(e.target.value)}
                                     />
-                                ))
-                            }
-                        </div>
-                    </div>
+                                    <button className="bg-[var(--primary)] min-w-[100px] p-3 rounded-xl text-white cursor-pointer hover:opacity-70 
+                                    transition-all duration-200 ease-linear w-fit disabled:cursor-not-allowed" 
+                                    disabled={search ? false : true}
+                                    onClick={() => searchLessons()}
+                                    >Search</button>
+                                    <div className="flex flex-col justify-center items-center p-3 h-24 w-fit rounded-xl bg-gray-100">
+                                        <div className="font-bold">Level:</div>
+                                        <div className="text-center">{grade ? grade : 'Loading...'}</div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="flex flex-col p-12 gap-4 items-center"> 
+                                <h1 className="text-3xl font-bold">{grade ? grade : 'Your'} Lessons</h1>
+                                <div className="w-full flex flex-wrap justify-center gap-4">
+                                    { 
+                                        filteredLessons && 
+                                        filteredLessons?.map((lesson) => ( 
+                                            <LessonCard
+                                            title={lesson.title}
+                                            description={lesson.description}
+                                            tags={[grade, lesson.isPaid ? 'Paid' : 'Free']}
+                                            price={lesson.price}
+                                            key={lesson._id}
+                                            lessonId={lesson._id}
+                                            token={token}
+                                            />
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                        </>
+                    }
                 </div>
             <Footer />
         </div>
